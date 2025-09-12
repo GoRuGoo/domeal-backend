a049e82e033d96f64808cfd37699273fd60a4426
GoRuGoo <dachanyr8989@gmail.com>
2025-09-13 04:07:52 +0900

    feat: initializeDBに環境変数を使えるように

--- api/model/initialize.go
+++ api/model/initialize.go
@@ -3,17 +3,18 @@ package model
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
 func InitDB() (*sql.DB, error) {
 	var conn *sql.DB
-	user := "postgres"
-	pass := "postgres"
-	host := "db"
-	port := "5432"
-	name := "stg"
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, pass, name)
